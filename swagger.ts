3751e1d5ac0d9ab6180f1288a70c7b71b1be606b
hydrarguru <henrik@engqvist.org>
2024-10-15 13:15:28 +0200

    added swagger definition.

---
+++ swagger.ts
@@ -0,0 +1,43 @@
+export const swaggerDef = {
+    failOnErrors: true, // Whether or not to throw when parsing errors. Defaults to false.
+    definition: {
+      openapi: '3.0.3',
+      info: {
+        title: 'Reader API',
+        version: '1.0.0',
+      },
+      "termsOfService": "https://www.github.com/hydrarguru/reader-backend",
+      "contact": {
+      "name": "Contact information",
+      "url": "https://www.github.com/hydrarguru",
+      "email": "henrik@engqvist.org"
+      },
+      servers: [
+        {
+          url: 'http://localhost:10000',
+        },
+        {
+          url: 'https://reader-backend-oank.onrender.com',
+        }
+      ],
+      tags: [
+        {
+          name: 'Misc',
+          description: 'Misc endpoints.',
+        },
+        {
+          name: 'User',
+          description: 'User related endpoints.',
+        },
+        {
+          name: 'Post',
+          description: 'Post related endpoints.',
+        },
+        {
+          name: 'Community',
+          description: 'Community related endpoints.',
+        },
+      ],
+    },
+    apis: ['./src/routes/*.ts'],
+  };
