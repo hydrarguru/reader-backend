1dbf16960684db6ab6b1aabc157473509ff2e172
Henrik Engqvist <henrik@engqvist.org>
2025-07-15 22:28:10 +0200

    Refactor/routes (#9)
    * refactor: small changes to endpoints.
    
    * refactor: another small changes to endpoints.

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -7,7 +7,7 @@ export const UserRouter = express.Router();
 
 /**
  * @openapi
- * /users:
+ * /user/all:
  *   get:
  *     tags: [User]
  *     description: returns all users.
@@ -17,20 +17,20 @@ export const UserRouter = express.Router();
  *       404:
  *         description: Error fetching users.
  */
-UserRouter.get("/users", async (req, res) => {
-    const users = await getAllUsers();
-    if(users === null) {
-      res.status(404).send("Error fetching users.");
-    }
-    else {
-      res.send(users);
-    }
+UserRouter.get("/user/all", async (req, res) => {
+  const users = await getAllUsers();
+  if (users === null) {
+    res.status(404).send("Error fetching users.");
+  }
+  else {
+    res.send(users);
+  }
 });
 
 
 /**
  * @openapi
- * /users/{id}:
+ * /user/{id}:
  *   get:
  *     tags: [User]
  *     description: returns a user by id.
@@ -40,29 +40,29 @@ UserRouter.get("/users", async (req, res) => {
  *       404:
  *         description: User not found.
  */
-UserRouter.get("/users/:id", async (req, res) => {
-    if (!validateUUID(req.params.id)) {
-      res.status(400).send("Invalid UUID.");
+UserRouter.get("/user/:id", async (req, res) => {
+  if (!validateUUID(req.params.id)) {
+    res.status(400).send("Invalid UUID.");
+    return;
+  }
+  else {
+    const id = req.params.id;
+    const user = await getUser(id);
+    if (user === null) {
+      res.status(404).send("User not found.");
       return;
     }
     else {
-      const id = req.params.id;
-      const user = await getUser(id);
-      if(user === null) {
-        res.status(404).send("User not found.");
-        return;
-      }
-      else {
-        res.send(user);
-      }
+      res.send(user);
     }
+  }
 });
 
 
 
 /**
  * @openapi
- * /users/create:
+ * /user/create:
  *   post:
  *     tags: [User]
  *     description: creates a user.
@@ -85,20 +85,20 @@ UserRouter.get("/users/:id", async (req, res) => {
  *       500:
  *         description: Error creating user.
  */
-UserRouter.post('/users/create', async (req, res) => {
-    const newUser: User = {
-        user_id: crypto.randomUUID(),
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email
-    };
-    await insertOne('Users', newUser).catch((err) => {
-        console.error(err);
-        res.status(500).send({
-          message: 'Error creating user.',
-          error: err
-        });
-    }).finally(() => {
-        res.status(201).send('User created.');
+UserRouter.post('/user/create', async (req, res) => {
+  const newUser: User = {
+    user_id: crypto.randomUUID(),
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email
+  };
+  await insertOne('Users', newUser).catch((err) => {
+    console.error(err);
+    res.status(500).send({
+      message: 'Error creating user.',
+      error: err
     });
+  }).finally(() => {
+    res.status(201).send('User created.');
+  });
 });
